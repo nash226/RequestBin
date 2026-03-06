daf500eddb8b2da456a0e7801a59f7302166e8ab
lewisian8787 <lewisian8787@gmail.com>
2026-03-06 16:28:16 -0500

    Initial server setup and test

--- back_end/build/index.js
+++ back_end/build/index.js
@@ -1,3 +1,25 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import express from 'express';
+import mongoose from 'mongoose';
+// import pg from 'pg';
+const app = express();
+const PORT = process.env.PORT || 3000;
+//Middleware
+app.use(express.json()); // JSON bodies
+app.use(express.urlencoded({ extended: true })); // URL-encoded bodies
+app.use(express.text({ type: 'text/*' })); // Text bodies
+//Routes
+app.all('/:id', (req, res) => {
+    const data = {
+        method: req.method,
+        path: req.path,
+        headers: req.headers,
+        body: req.body
+    };
+    console.log(data);
+});
+//Error Handler
+//Start Server
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+});
 //# sourceMappingURL=index.js.map
---
+++ back_end/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+// import pg from 'pg';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.json()); // JSON bodies
+app.use(express.urlencoded({ extended: true })); // URL-encoded bodies
+app.use(express.text({ type: 'text/*' })); // Text bodies
+
+//Routes
+app.all('/:id', (req, res) => {
+  const data = {
+    method: req.method,
+    path: req.path,
+    headers: req.headers,
+    body: req.body
+  }
+})
+
+//Error Handler
+
+//Start Server
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`)
+})
