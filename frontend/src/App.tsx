d8fc274baf6bbb462bab03970c2805e6cd0fd56b
jonnorrie <jonathontirabassi@gmail.com>
2026-03-07 17:46:18 -0500

    added fetch with token data to revtrieve baskets and generate token from backend if one not given

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
 import NewBasket from './components/NewBasket'
 import MyBaskets from './components/MyBaskets'
 
 function App() {
+
+  const [userToken, setUserToken] = useState<string | null>(null)
+  const [baskets, setUserBaskets] = useState(null)
+
+  useEffect(() => {
+    let token = localStorage.getItem('userToken') || null
+    setUserToken(token);
+
+    fetch("/api/web/baskets", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ token: token }),
+    })
+      .then(response => response.json())
+      .then((data: { token: string, baskets: any }) => {
+        localStorage.setItem("userToken", data.token);
+        setUserToken(data.token);
+        setUserBaskets(data.baskets);
+      })
+      .catch(err => {
+        console.error("An error occurred", err);
+      });
+
+  }, []);
+
   return (
     <div className="App">
       <div className="component-container">
